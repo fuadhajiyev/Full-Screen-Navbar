72503afd3cba2939c556880f91e31eea7cd6c5d4
Fuad Hajiyev <hajiyev1996@gmail.com>
2019-05-18 21:25:50 +0400

    first navbar commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function openNav(){
+
+    document.getElementById('open').style.display='none';
+    document.getElementById('nav').style.width='100%';
+}
+
+function closeNav(){
+
+    document.getElementById('nav').style.width='0';
+    document.getElementById('open').style.display="block";
+}
+
+
+
+    
